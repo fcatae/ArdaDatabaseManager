f33c6f64b49e4aa7592e3c2e9de50badefefc4d9
Fabricio Catae <fcatae@microsoft.com>
2017-06-03 07:51:36 -0300

    organize script manager

--- ArdaDbMgr/Managers/ScriptManager.cs
+++ ArdaDbMgr/Managers/ScriptManager.cs
@@ -12,13 +12,17 @@ namespace ArdaDbMgr.Managers
     {
         private readonly IFileServices _fileSvcs;
         private bool _isInitialized = false;
-        private SortedDictionary<int, SqlScript> _index = new SortedDictionary<int, SqlScript>();
-        private List<SqlScript> _otherScripts = new List<SqlScript>();
+        private SortedDictionary<int, SqlScript> _index;
+        private List<SqlScript> _otherScripts;
         private int _maxIndex = -1;
 
         public int MaxIndex
-            {
-            get { return _maxIndex;  }}
+        {
+            get {
+                CheckInitialize();
+                return _maxIndex;
+            }
+        }
 
         public ScriptManager(IFileServices fileSvcs)
         {
@@ -27,6 +31,9 @@ namespace ArdaDbMgr.Managers
 
         public void Init()
         {
+            _index = new SortedDictionary<int, SqlScript>();
+            _otherScripts = new List<SqlScript>();
+
             // enumerate all the scripts
             var scripts = _fileSvcs.EnumerateFiles();
 
@@ -34,42 +41,22 @@ namespace ArdaDbMgr.Managers
             {
                 int idx = GetIndex(script.Name);
 
-                SetMaxIndex(idx);
+                if (idx <= 0)
+                {
+                    ReportInvalidScript(script);
+                    continue;
+                }
+
                 RegisterScript(idx, script);
             }
             
             _isInitialized = true;
         }
 
-        void SetMaxIndex(int index)
-        {
-            if (index > _maxIndex)
-                _maxIndex = index;
-        }
-
-        void RegisterScript(int index, SqlScript script)
-        {
-            if (index > 0)
-            {
-                // valid script
-                _index.Add(index, script);
-            }
-            else
-            {
-                _otherScripts.Add(script);
-            }
-        }
-
-        void CheckInitialize()
-        {
-            if(!_isInitialized)
-            {
-                Init();
-            }
-        }
-        
         public string ReadScript(int index)
         {
+            CheckInitialize();
+
             if (!_index.ContainsKey(index))
                 return null;
 
@@ -89,6 +76,26 @@ namespace ArdaDbMgr.Managers
 
             return index;
         }
-        
+
+        void RegisterScript(int index, SqlScript script)
+        {
+            if (index > _maxIndex)
+                _maxIndex = index;
+
+            _index.Add(index, script);
+        }
+
+        void ReportInvalidScript(SqlScript script)
+        {
+            _otherScripts.Add(script);
+        }
+
+        void CheckInitialize()
+        {
+            if(!_isInitialized)
+            {
+                Init();
+            }
+        }        
     }
 }
